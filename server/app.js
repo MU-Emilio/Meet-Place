317f931106447f777d0c185571da0115829c35da
Emilio Gonzalez Martinez <emiliogonmar@fb.com>
2022-07-06 10:11:54 -0700

    App keys in .env

--- server/app.js
+++ server/app.js
@@ -1,45 +1,45 @@
+require('dotenv').config()
 const Parse = require('parse/node');
 const express = require('express')
 const app = express()
 app.use(express.json());
+const APP_ID = process.env.APP_ID;
+const JS_KEY = process.env.JS_KEY;
+const MASTER_KEY = process.env.MASTER_KEY;
 
 Parse.serverURL = 'https://parseapi.back4app.com'; 
 Parse.initialize(
-  'yfMNoZWBaXbFbmwoo9xOGWwraxRQtIKOqoW0pEkJ', // Application ID
-  'MzA9OJbSEboIaCBImjZFWd0FbsdNsUVtHWvO6IIX', // Javascript key
-  'LmLxVrYCktqVbfkooRkw15UTZngbZ0yiFmmk2ObY' //  Master key
+  APP_ID, // Application ID
+  JS_KEY, // Javascript key
+  MASTER_KEY //  Master key
 );
-
-app.get('/', (req, res) => {
-    res.json({"ping" : "pong"})
-})
 
 // Request the Log in passing the email and password
 app.post('/users/login', async(req, res) => {
-    let infoUser = req.body;
+    const payload = req.body;
     
     try{
-      let user = await Parse.User.logIn(infoUser.usernameLogin, infoUser.passwordLogin)
-      res.send({ message: "User logged!", status: "success",  infoUser: infoUser });
+      const user = await Parse.User.logIn(payload.usernameLogin, payload.passwordLogin)
+      res.send({ message: "User logged!", status: "success",  payload: payload });
     } catch (error){
-      res.send({ message: error.message, status: "danger",  infoUser: infoUser});
+      res.send({ message: error.message, status: "danger",  payload: payload});
     }
   });
 
 // Register the user passing the username, password and email
 app.post('/users/register', async(req, res) => {
-    let infoUser = req.body;    
-    let user = new Parse.User();
+    const payload = req.body;    
+    const user = new Parse.User();
   
-    user.set("username", infoUser.usernameRegister);
-    user.set("password", infoUser.passwordRegister);
-    user.set("email", infoUser.emailRegister);
+    user.set("username", payload.usernameRegister);
+    user.set("password", payload.passwordRegister);
+    user.set("email", payload.emailRegister);
   
     try{
       await user.signUp();
-      res.send({ message: "User created!", status: "success",  infoUser: infoUser});
+      res.send({ message: "User created!", status: "success",  payload: payload});
     } catch (error) {
-      res.send({ message: error.message, status: "danger",  infoUser: infoUser});
+      res.send({ message: error.message, status: "danger",  payload: payload});
     }
   });
 
