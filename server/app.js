9c2a614dbd9a0940f6680933f16ea00ce8c955a2
Emilio Gonzalez Martinez <emiliogonmar@fb.com>
2022-07-05 21:17:38 -0700

    Login endpoint

--- server/app.js
+++ server/app.js
@@ -14,6 +14,19 @@ app.get('/', (req, res) => {
     res.json({"ping" : "pong"})
 })
 
+// Request the Log in passing the email and password
+app.post('/users/login', async(req, res) => {
+    let infoUser = req.body;
+    
+    try{
+      let user = await Parse.User.logIn(infoUser.usernameLogin, infoUser.passwordLogin)
+      res.send({ loginMessage: "User logged!", RegisterMessage: '', typeStatus: "success",  infoUser: infoUser });
+    } catch (error){
+      res.send({ loginMessage: error.message, RegisterMessage: '', typeStatus: "danger",  infoUser: infoUser});
+    }
+  });
+
+// Register the user passing the username, password and email
 app.post('/users/register', async(req, res) => {
     let infoUser = req.body;    
     let user = new Parse.User();
