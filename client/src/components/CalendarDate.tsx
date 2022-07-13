9ec49eac1125ff70d962d0dac616827a9151671f
Emilio González Martínez <emiliogonmar2002@gmail.com>
2022-07-13 15:31:17 -0700

    Actual month highlight

--- client/src/components/CalendarDate.tsx
+++ client/src/components/CalendarDate.tsx
@@ -1,14 +1,31 @@
-import { format } from "date-fns";
-import React from "react";
-
+import { startOfDay, format } from "date-fns";
 interface Props {
   date: Date;
 }
 
+const styles = {
+  today: {
+    color: "blue",
+  },
+  notToday: {
+    color: "black",
+  },
+};
+
 const CalendarDate = ({ date }: Props) => {
   return (
     <div>
-      <p className="border h-20 px-1">{format(date, "dd")}</p>
+      <p
+        className="border h-20 px-1"
+        style={
+          format(date, "MM/dd/yyyy") ==
+          format(startOfDay(new Date()), "MM/dd/yyyy")
+            ? styles.today
+            : styles.notToday
+        }
+      >
+        {format(date, "dd")}
+      </p>
     </div>
   );
 };
