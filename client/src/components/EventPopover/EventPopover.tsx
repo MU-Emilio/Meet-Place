66d67516f437a7f66f08f3c76633bdebc6d0ce84
Emilio González Martínez <emiliogonmar2002@gmail.com>
2022-07-21 20:34:54 -0700

    Delete event button with POST request

--- client/src/components/EventPopover/EventPopover.tsx
+++ client/src/components/EventPopover/EventPopover.tsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { SESSION_KEY } from "../../lib/constants";
 import { useQuery } from "react-query";
 import GuestList from "../GuestList";
+import { useMutation, useQueryClient } from "react-query";
 interface Props {
   event: EventType | null;
   isHover: boolean;
@@ -27,9 +28,40 @@ const EventPopover = ({ event, isHover }: Props) => {
     fetchGuests
   );
 
+  const deleteEvent = async () => {
+    const { data: response } = await axios.post(
+      "http://localhost:3001/event/delete",
+      {
+        event: event,
+      },
+      {
+        headers: {
+          authorization: localStorage.getItem(SESSION_KEY) || false,
+        },
+      }
+    );
+    return response.data;
+  };
+
+  const queryClient = useQueryClient();
+
+  const { mutate, isLoading: deleteLoading } = useMutation(deleteEvent, {
+    onError: () => {
+      alert("there was an error");
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(["events"]);
+    },
+  });
+
   return (
     <div className={`eventPop ${isHover ? "fadeIn" : "fadeOut"}`}>
-      <h1>{event?.title}</h1>
+      <div className="flex justify-between">
+        <h1>{event?.title}</h1>
+        <button className=" bg-red-300" onClick={() => mutate()}>
+          Delete
+        </button>
+      </div>
       <hr />
       <p>Date: {event?.date.iso}</p>
       <p>{event?.description}</p>
