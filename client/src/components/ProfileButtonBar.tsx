2d855f74e6d09c9f0eb12cc67f814e5d55a6e3d4
Emilio González Martínez <emiliogonmar2002@gmail.com>
2022-07-24 17:21:03 -0700

    Initial component

--- client/src/components/ProfileButtonBar.tsx
+++ client/src/components/ProfileButtonBar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { User } from "../lib/types";
 import Logout from "./Logout";
 
@@ -11,11 +12,16 @@ const ProfileButtonBar = ({ user }: Props) => {
 
   user ? (profileImage = user.profileImage.url) : (profileImage = null);
 
+  const navigate = useNavigate();
+
   return (
-    <div className="h-full w-[250px] bg-gray-200 flex px-2 py-1 rounded-md shadow-md items-center">
+    <div className="h-full w-[250px] bg-gray-200 flex px-2 py-1 rounded-md shadow-md items-center cursor-pointer">
       <div className=" px-2 flex justify-around gap-4 mr-0 items-center">
         <Logout />
-        <div className="flex justify-between gap-3 hover:scale-105 ease-in-out duration-300 bg-white rounded-md shadow-md p-1 w-[90px]">
+        <div
+          className="flex justify-between gap-3 hover:scale-105 ease-in-out duration-300 bg-white rounded-md shadow-md p-1 w-[90px]"
+          onClick={() => navigate("/profile")}
+        >
           <p className="font-medium">{user.username}</p>
           <div className="w-fit">
             {profileImage && (
