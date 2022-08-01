5b18e167145ba3050f6e3f26a8ab9c500f028c34
Emilio González Martínez <emiliogonmar2002@gmail.com>
2022-08-01 10:35:18 -0700

    User Feed Events Container component

--- client/src/components/EventsFeed.tsx
+++ client/src/components/EventsFeed.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { User } from "../lib/types";
+import UserFeedEventsContainer from "./UserFeedEventsContainer";
 
 interface Props {
   userId: string;
@@ -16,7 +17,7 @@ const EventsFeed = ({ userId, pages }: Props) => {
         <div
           className={`h-[40px] min-w-[40px] ${
             i === currentPage ? "bg-blue-200" : "bg-gray-300"
-          } p-1 flex items-center cursor-pointer hover:scale-105 ease-in-out duration-300`}
+          } p-1 flex items-center cursor-pointer hover:bg-blue-300 hover:scale-105 ease-in-out duration-300`}
           onClick={() => setCurrentPage(i)}
         >
           <p className="m-auto w-fit">{i}</p>
@@ -27,7 +28,8 @@ const EventsFeed = ({ userId, pages }: Props) => {
 
   return (
     <div className=" bg-gray-200 w-[1000px] h-[700px] p-5">
-      <div className="bg-gray-300 h-[600px] w-[950px] m-auto"></div>
+      <UserFeedEventsContainer page={currentPage} />
+
       <div className="w-[950px] h-[50px] bg-white flex gap-3 overflow-x-auto items-center p-2 m-auto">
         {page_cards}
       </div>
