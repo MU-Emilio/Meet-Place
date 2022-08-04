caa61541ab2f4dcb9fe64a480213fe2790a65bfb
Emilio González Martínez <emiliogonmar2002@gmail.com>
2022-08-03 19:28:58 -0700

    Sort users accepted/pending/rejected

--- client/src/components/GuestList.tsx
+++ client/src/components/GuestList.tsx
@@ -15,21 +15,24 @@ const GuestList = ({ guests, horizontal }: Props) => {
           horizontal ? "h-full" : " h-[220px]"
         } overflow-y-auto whitespace-nowrap scroll-smooth`}
       >
-        {guests.map((item, index) => (
-          <React.Fragment key={index}>
-            <div
-              className={`${
-                horizontal && "inline-block"
-              } cursor-pointer hover:scale-105 ease-in-out duration-300 mr-2 px-2`}
-            >
-              <UserCard
-                userCard={item.guest}
-                ButtonComponent={null}
-                status={item.status}
-              />
-            </div>
-          </React.Fragment>
-        ))}
+        {guests.map(
+          (item, index) =>
+            item && (
+              <React.Fragment key={index}>
+                <div
+                  className={`${
+                    horizontal && "inline-block"
+                  } cursor-pointer hover:scale-105 ease-in-out duration-300 mr-2 px-2`}
+                >
+                  <UserCard
+                    userCard={item.guest}
+                    ButtonComponent={null}
+                    status={item.status}
+                  />
+                </div>
+              </React.Fragment>
+            )
+        )}
       </div>
     </div>
   );
