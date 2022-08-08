34cc3b584afe0f7fb65d0ed769cac9a75fc450f2
Emilio González Martínez <emiliogonmar2002@gmail.com>
2022-08-08 10:38:09 -0700

    Login style

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -56,14 +56,14 @@ const Login = () => {
   };
 
   return (
-    <div className="h-[800px] p-[200px]">
+    <div className="h-[900px] p-[200px] border-t-8 border-secundary rounded-md">
       <div
-        className="block bg-primary rounded-lg mx-auto mb-0"
+        className="bloc rounded-lg mx-auto mb-0 shadow-lg border-t-8 border-primary"
         style={styles.formContainer}
       >
         <div className=" bg-white p-5">
           <Logo />
-          <h1 className="text-center font-medium text-4xl">Login</h1>
+          <h1 className="text-center font-medium text-4xl">Log In</h1>
         </div>
 
         <Formik
@@ -81,7 +81,7 @@ const Login = () => {
                 Username
               </label>
               <Field
-                className="block mb-5 w-full"
+                className="block mb-5 w-full p-1 rounded-sm border-2"
                 id="username"
                 name="username"
                 placeholder="Username"
@@ -91,7 +91,7 @@ const Login = () => {
             <div className="block mb-5" style={styles.field}>
               <label htmlFor="password">Password</label>
               <Field
-                className="block w-full"
+                className="block w-full p-1 rounded-sm border-2"
                 id="password"
                 name="password"
                 placeholder="Password"
@@ -99,16 +99,31 @@ const Login = () => {
               />
             </div>
 
-            <button className="bg-white px-3" type="submit">
-              Submit
-            </button>
+            <div>
+              <div className="mx-auto w-fit">
+                <button
+                  className="bg-primary p-2 px-3 w-[150px] rounded-md text-white font-medium"
+                  type="submit"
+                >
+                  Enter
+                </button>
+                <div className="flex m-auto justify-between items-center mt-4">
+                  <hr className="border-[1px] w-[50px]" />
+                  <p className="text-gray-200 font-bold">Or</p>
+                  <hr className="border-[1px] w-[50px]" />
+                </div>
+                <button
+                  className="bg-primary p-2 px-3 w-[150px] rounded-md text-white font-medium mt-4"
+                  type="button"
+                  onClick={() => navigate("/register")}
+                >
+                  Sign Up
+                </button>
+              </div>
+            </div>
           </Form>
         </Formik>
         {submitMessage && <p>{submitMessage}</p>}
-        <p>Need an account?</p>
-        <p className=" underline" onClick={() => navigate("/register")}>
-          Sign up
-        </p>
       </div>
     </div>
   );
